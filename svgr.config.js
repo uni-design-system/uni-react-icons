c8635564651cad29d505b621dbfd9d791d5f80e8
George English <gaenglish@gmail.com>
2022-03-29 16:11:33 -0400

    updating build template

--- svgr.config.js
+++ svgr.config.js
@@ -2,9 +2,31 @@ module.exports = {
   icon: true,
   expandProps: false,
   titleProp: true,
-  typescript: false,
+  typescript: true,
+  dimensions: false,
+  svgProps: {
+    'height': '{height}',
+    'width': '{width}'
+  },
+  template: (variables, { tpl }) => {
+    return tpl`
+      ${variables.imports};
+      interface SVGRProps {
+        title?: string;
+        titleId?: string;
+        color?: string;
+        height?: string;
+        width?: string;
+      }
+      const ${variables.componentName} = ({ title, titleId, color, height = '24px', width = '24px' }: SVGRProps) => (
+        ${variables.jsx}
+      );
+       
+      ${variables.exports};
+    `
+  },
   replaceAttrValues: {
-    '#000': 'currentColor'
+    '#d1d1d1': '{color}'
   },
   outDir: 'src'
 }
